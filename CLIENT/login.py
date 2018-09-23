b0924e2f4a50992a277497adb77504543f1a69c1
Aslam Muhammed <aslamsmuhammed1@gmail.com>
2018-09-23 09:42:35 +0530

    Update login.py

--- CLIENT/login.py
+++ CLIENT/login.py
@@ -4,7 +4,7 @@
 #
 # Created by: PyQt5 UI code generator 5.9
 #
-# WARNING! All changes made in this file will be lost!
+# WARNING! All changes made in this file will be lost
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
